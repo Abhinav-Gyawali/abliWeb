ced4367b4f15281b61af30ee23252387ca916409
Abhinav-Gyawali <publicgyawali@gmai.com>
2023-09-28 20:36:20 +0545

    urls

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/',views.home,name="home"),
+    path('',views.home,name="home"),
     path('password-reset/verify', views.handle_password_reset, name='handle_password_reset'),
 ]
