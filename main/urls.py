614891943e2c543396d4799a0bc4fe3ec384b424
Abhinav-Gyawali <publicgyawali@gmai.com>
2023-09-28 20:26:03 +0545

    urls

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/',view.home,name="home"),
+    path('/',views.home,name="home"),
     path('reset-password/<str:oob_code>/', views.handle_password_reset, name='handle_password_reset'),
 ]
