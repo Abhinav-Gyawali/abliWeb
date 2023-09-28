24753fa52948ca0d8e0a8f0b0274010f2fd4417f
Abhinav-Gyawali <publicgyawali@gmai.com>
2023-09-28 20:35:06 +0545

    urls

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('/',views.home,name="home"),
-    path('reset-password/<str:oob_code>/', views.handle_password_reset, name='handle_password_reset'),
+    path('password-reset/verify', views.handle_password_reset, name='handle_password_reset'),
 ]
