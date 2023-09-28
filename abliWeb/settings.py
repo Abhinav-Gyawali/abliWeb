131e023f428b099fa4fb07a3f58c30840d20023d
Abhinav-Gyawali <publicgyawali@gmai.com>
2023-09-28 19:08:41 +0545

    reworked

--- abliWeb/settings.py
+++ abliWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vxv*6a_-%jz^b8-@^tjz=(1cktcf)rw)e(#u$!0@_ao((*xzlb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
